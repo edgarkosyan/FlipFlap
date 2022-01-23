c455d9a9ef0874e6b0e051ce900a1c9b176ead8f
edgar <e.kosyan0073@gmail.com>
2022-01-23 11:45:19 +0400

    Initial Commit

---
+++ FlipFlap/FlipFlapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipFlapApp.swift
+//  FlipFlap
+//
+//  Created by Edgar on 23.01.22.
+//
+
+import SwiftUI
+
+@main
+struct FlipFlapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
